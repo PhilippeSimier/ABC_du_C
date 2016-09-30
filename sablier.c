f79bf240d6331da5438ecf94043b4298c05af0cc
philippe SIMIER <philaure@wanadoo.fr>
2016-09-30 14:31:31 +0200

    Sablier de nombres

--- sablier.c
+++ sablier.c
@@ -1,11 +1,21 @@
-/********************************************************************
-*	  Programme sablier de nombres		    		    *
-*  Nous pouvons représenter un sablier comme ci-dessous avec une    *
-*  suite de nombres dont le plus grand sur la première ligne doit   *
-*  être impair. Par exemple, ici Nbligne  vaut 7.		    *
-*
-*   
-*********************************************************************/
+/************************************************************************
+*	    SABLIER de NOMBRES			    		    	*
+*  Nous pouvons représenter un sablier comme ci-dessous avec une    	*
+*  suite de nombres dont le plus grand sur la première ligne doit   	*
+*  être impair. Par exemple, ici Nbligne  vaut 9.		    	*
+*									*
+*   01 02 03 04 05 06 07 08 09						*
+*      10 11 12 13 14 15 16						*
+*         17 18 19 20 21						*
+*            22 23 24							*
+*               25							*
+*            26 27 28							*
+*         29 30 31 32 33						*
+*      34 35 36 37 38 39 40						*
+*   41 42 43 44 45 46 47 48 49						*
+*									*
+*   									*
+*************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 

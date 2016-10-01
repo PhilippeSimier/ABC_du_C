c166f42f9576ce13ffc3aa58fd068cd770298c6e
PhilippeSimier <philaure@wanadoo.fr>
2016-10-01 11:07:51 +0200

    Calcul de pi

--- calcul_pi.c
+++ calcul_pi.c
@@ -1,32 +1,39 @@
 /****************************************************************
-*    Calcul d'une valeur approchée de PI			*
+*    Calcul d'une valeur approchée de pi			*
 *    En mathématique, nous avons appris que la somme :		*
-*    1 – 1/3 + 1/5 – 1/7 + 1/9 – 1/11 + 1/13 … tend vers pi/4,	*
-*	Compilation : gcc calcul_pi.c -o calcul_pi -Wall	*
-*       Exécution   : time ./calcul_pi				*
+*    1 – 1/3 + 1/5 – 1/7 + 1/9 – 1/11 + 1/13 ... tend vers pi/4 *
+*    4 - 4/3 + 4/5 - 4/7 + 4/9 - 4/11 + 4/13 ... Tend vers pi   *
+*								*
+*    Compilation : gcc calcul_pi.c -o calcul_pi -Wall		*
+*    Exécution   : time ./calcul_pi				*
+*								*
+*    Pi = 3.14159266 à +-0.00000001				*
+*								*
+*    real    0m10.730s						*
+*    user    0m10.720s						*
+*    sys     0m0.000s						*
+*								*
 *****************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
-#define EPSILON 0.000001
+#define EPSILON 0.00000001
 
 int main()
 {
-    double piSurQuatre = 1.0;
-    double signe = -1;
-    double denominateur = 3.0;
+    double pi = 4.0;
+    double signe = +1;
+    double denominateur = 1.0;
     double fraction;
 
-    piSurQuatre = 1.0;
-
     do {
-            fraction = 1.0/denominateur;
-            piSurQuatre +=  (fraction *  signe);
-            signe *= -1;
-            denominateur +=  2.0;
+        signe *= -1;
+        denominateur +=  2.0;
+	fraction = 4.0/denominateur;
+        pi +=  (fraction *  signe);
     }
-    while (fraction*4 > EPSILON);
+    while (fraction > EPSILON);
 
-    printf("Pi = %f\n", piSurQuatre*4);
+    printf("Pi = %.8f à +-%.8f\n", pi, fraction);
     return 0;
 }

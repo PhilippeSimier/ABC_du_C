2e1079a54040016625a3870c6ff7307d42accfce
PhilippeSimier <philaure@wanadoo.fr>
2016-10-20 20:00:25 +0200

    options en ligne de commande

---
+++ get_options/getopt.c
@@ -0,0 +1,44 @@
+/*************************************************************
+   Programme demo pour le passage d'argument en ligne de
+   commande. Ici deux arguments seront nécessaires -p -r
+   les caratères sont suivits par deux points,
+   les options s'attendent donc  à avoir un argument,
+
+   Compilation : gcc getopt.c -o getopt -Wall
+   Test        : $./getopt -p 50000 -r /home/
+   ou          : $./getopt
+   ou	       : $./getopt -r /home/pi
+*************************************************************/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include <unistd.h>  // pour getopt
+
+int main(int argc, char* argv[])
+{
+    char *racine;
+    int  port = 8888;
+    int c = 0;
+
+    // valeur par défaut pour les arguments: racine = "~" et port 8888
+    racine = getenv("PWD");
+
+    // Analyse des arguments r et p de la ligne de commande
+    while ((c = getopt (argc, argv, "r:p:")) != -1)
+        switch (c)
+        {
+            case 'r':
+                racine = malloc(strlen(optarg));
+                strcpy( racine ,optarg);
+                break;
+            case 'p':
+                port   = atoi(optarg);  // conversion en entier
+                break;
+            default:
+		printf("usage: getopt [-r chemin] [-p port] \n");
+                exit(1);
+        }
+
+    printf("Serveur démarré sur le port no. %s%d%s avec répertoire racine as %s%s%s\n", "\033[92m", port, "\033[0m","\033[92m", racine, "\033[0m");
+    return 0;
+}

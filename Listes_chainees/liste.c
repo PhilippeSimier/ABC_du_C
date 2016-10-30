6cd3874a72de9a604abfc484089c9753d7c7d148
PhilippeSimier <philaure@wanadoo.fr>
2016-10-30 10:55:00 +0100

    ajout de liste

--- Listes_chainees/liste.c
+++ Listes_chainees/liste.c
@@ -1,3 +1,6 @@
+/******************************************************************************
+*  gcc liste.c -o liste -Wall
+******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,18 +21,20 @@ void push(Cellule **oldL, float donnee){
 }
 
 // retire une valeur en queue de  pile
-float pull(Cellule **L){
-    float donnee;
+// la fonction renvoie 1 en cas d'erreur ou 0 en cas de succès
+// la pile est passé par adresse on a doonc un double pointeur
+int pull(Cellule **L, float *donnee){
+
     Cellule *pointeur;
     if (*L == NULL)
-	return 0.0;
+	return 1;  // si la pile est vide retourne 1
 
-    donnee = (*L)->donnee;
+    *donnee = (*L)->donnee;
     pointeur = (*L)->next;
     free(*L);  //destruction de la cellule retirée
     *L = pointeur;
 
-    return donnee;
+    return 0;
 }
 
 // Affichage du contenu de la liste
@@ -48,26 +53,68 @@ int len(Cellule *L){
 	nb++;
     return nb;
 }
+// pour détruire une liste chainée il faut détruire chacune des cellules
+void detruire(Cellule **L){
+    Cellule *pointeur;
+
+    while( *L != NULL){
+	pointeur = *L;   // mémorisation de l'adresse de la cellule
+        *L = (*L)->next; // passage a la cellule suivante
+        free(pointeur);  // destruction de cellule  mémorisée
+    }
+    *L = NULL;
+}
 
 int main()
 {
     float valeur;
     Cellule *liste = NULL; //initialisation d'une liste vide
-    int i;
+    int i, erreur;
     float tableau[4] = { 6.5, 4.23, 2.1, 4.5 };
+    char choix;
 
     for (i=0 ; i<4 ; i++){
     	push( &liste, tableau[i]);
     }
 
     afficher(liste);
-    printf ("taille de la liste : %d\n", len(liste));
+    printf ("taille de la liste : %d\n\n", len(liste));
 
-    for (i=0 ; i<4 ; i++){
-   	valeur = pull( &liste);
-   	printf("Valeur retirée %.2f\n", valeur);
-   	afficher(liste);
-   	printf ("taille de la liste : %d\n", len(liste));
+    printf(" d pour détruire la liste\n");
+    printf(" a pour ajouter un élément\n");
+    printf(" r pour retirer un élément\n");
+    printf(" s pour afficher le contenu\n");
+    printf(" q pour quitter\n");
+
+    do{
+    	printf("Votre choix ? :");
+    	scanf(" %c", &choix);
+
+    	switch(choix){
+    	case 'd':
+            detruire(&liste);
+            break;
+
+        case 'r':
+            erreur = pull( &liste, &valeur);
+            if(erreur == 0)
+            	printf("Valeur retirée %.2f\n", valeur);
+            else
+            	printf("Erreur liste vide\n");
+            afficher(liste);
+            printf ("taille de la liste : %d\n", len(liste));
+            break;
+
+	case 'a':
+	    printf("Valeur à ajouter :");
+ 	    scanf("%f",&valeur);
+	    push( &liste, valeur);
+	    break;
+
+	case 's':
+	    afficher(liste);
+        }
     }
+    while(choix != 'q');
     return 0;
 }

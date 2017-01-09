0aa2264e31f424550777af0a7031fe9d6213d619
PhilippeSimier <philaure@wanadoo.fr>
2017-01-09 10:55:53 +0100

    corrigé Q3

---
+++ 8_Acces_Fichiers/TD1/q3.c
@@ -0,0 +1,34 @@
+/***************************************************************
+*  Compter le nombre de lettres minuscules dans le fichier
+*  fichierTexte.txt
+*
+***************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <ctype.h>
+
+void testErreur(){
+    if(errno != 0){
+        printf("Erreur fichier : erreur n'%d\n", errno);
+        exit(0);
+    }
+}
+
+int main()
+{
+    FILE * fp;
+    char retour;
+    int cptlower = 0;
+    int cptupper = 0;
+
+    fp = fopen("fichierTexte.txt","r");
+    testErreur();
+    while (!feof(fp)){   // tant que pas la fin de fichier
+        retour = fgetc(fp);   // lecture d'un caractère
+        if (islower(retour)) cptlower++;   // si minuscule
+        if (isupper(retour)) cptupper++;   // si majuscule
+    }
+    printf("Minuscules = %d\nMajuscules = %d \n", cptlower, cptupper);
+    return 0;
+}

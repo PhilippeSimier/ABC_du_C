c2f568f50c4ad383f0ae2d418fccbe4a977497a1
PhilippeSimier <philaure@wanadoo.fr>
2017-04-18 11:12:36 +0200

    serie USB

--- 11_Liaison_serie/rfid.c
+++ 11_Liaison_serie/rfid.c
@@ -15,26 +15,46 @@
 #include <termios.h>
 #include <sys/ioctl.h>
 
-int configure(int modele){
+int ouvrirSerie(const char *device){
     int fd = -1;
-    system("clear"); // Efface l'écran et configure la liaison série
-    if (modele == 3){
-     	fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY);
-    }
-    else{
-     	fd = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY);
-    }
+
+    fd = open(device, O_RDWR | O_NOCTTY);
+
     if ( fd == -1 ) {
         printf("pb ouverture: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
-
     return fd;
 }
 
-void raw_mode (int fd)
+void configurerSerie (int fd, const int baud)
 {
     struct termios term;
+    speed_t myBaud;
+
+    switch (baud){
+    	case     50:        myBaud =     B50 ; break ;
+    	case     75:        myBaud =     B75 ; break ;
+    	case    110:        myBaud =    B110 ; break ;
+    	case    134:        myBaud =    B134 ; break ;
+    	case    150:        myBaud =    B150 ; break ;
+    	case    200:        myBaud =    B200 ; break ;
+    	case    300:        myBaud =    B300 ; break ;
+    	case    600:        myBaud =    B600 ; break ;
+    	case   1200:        myBaud =   B1200 ; break ;
+    	case   1800:        myBaud =   B1800 ; break ;
+    	case   2400:        myBaud =   B2400 ; break ;
+    	case   4800:        myBaud =   B4800 ; break ;
+    	case   9600:        myBaud =   B9600 ; break ;
+    	case  19200:        myBaud =  B19200 ; break ;
+    	case  38400:        myBaud =  B38400 ; break ;
+    	case  57600:        myBaud =  B57600 ; break ;
+    	case 115200:        myBaud = B115200 ; break ;
+    	case 230400:        myBaud = B230400 ; break ;
+
+    	default:
+      	    myBaud =   B9600;
+    }
 
     tcgetattr(fd, &term);
 
@@ -56,8 +76,8 @@ void raw_mode (int fd)
     /* 8 bits de données, pas de parité */
     term.c_cflag &= ~(PARENB | CSIZE);
     term.c_cflag |= CS8;
-    /* 9600 bauds */
-    term.c_cflag |= B9600;
+    /* vitesse de la transmission */
+    term.c_cflag |= myBaud;
     tcsetattr(fd, TCSANOW, &term);
 
     // play with DTR
@@ -88,10 +108,10 @@ void reception(int fd){
 int main(int argc, char **argv) {
     int retour, fd, i;
     char message[255];
-    int modele = 3; // 3 pour une rasberry pi3 et 2 pour pi2
 
-    fd = configure(modele);
-    raw_mode(fd);
+    system("clear");
+    fd = ouvrirSerie("/dev/ttyUSB0");
+    configurerSerie(fd, 9600);
     for (i=0; i < 40; i++){
 	printf("%d :\n", i); 
 	reception(fd);

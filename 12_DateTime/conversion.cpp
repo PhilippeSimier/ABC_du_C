efbb6d3714bc6c6298f1ea15b0a67798a162f19e
PhilippeSimier <philaure@wanadoo.fr>
2017-05-15 12:03:36 +0200

    difference entre 2 dates

--- 12_DateTime/conversion.cpp
+++ 12_DateTime/conversion.cpp
@@ -2,7 +2,9 @@
 *	Conversion d'une date sous forme de chaine
 *	en time-stamp
 *
-*       Compilation : g++ -Wall conversion.c -o conversion
+*       Compilation : g++ -Wall conversion.cpp -o conversion
+*       Test : en ligne http://www.timestamp.fr/
+*       Exemple : 2017-05-13 16:50:01   donne 1494683401
 *****************************************************************/
 
 #include <time.h>
@@ -10,17 +12,25 @@
 #include <string>    // pour strptime
 #include <string.h>  // pour memset
 
-int main(int argc, char **argv) {
+time_t dateToTimeStamp(std::string dateString){
 
     struct tm tm;
-    time_t ts = 0;
     memset(&tm, 0, sizeof(tm));
 
-    std::string dateString = "2017-05-13 15:50:01";
-
     strptime(dateString.c_str(), "%Y-%m-%d %T", &tm);
-    ts = mktime(&tm);
+    return mktime(&tm);
+}
 
-    printf("%d \n", (int)ts); //unix time-stamp
-    printf("%s \n", ctime(&ts)); //human readable date
+
+int main(int argc, char **argv) {
+
+    std::string date = "2017-05-15 10:55:01";
+    time_t retour, maintenant, difference;
+
+    retour = dateToTimeStamp(date);
+    time(&maintenant);
+    difference = maintenant - retour;
+
+    printf("%d s écoulées depuis le ",(int)difference); //unix time-stamp
+    printf("%s \n", ctime(&retour)); //human readable date
 }

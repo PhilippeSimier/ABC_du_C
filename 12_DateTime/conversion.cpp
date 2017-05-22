7a9ec0d016b82c12aa3a4409bd587066452df34b
PhilippeSimier <philaure@wanadoo.fr>
2017-05-22 11:56:05 +0200

    update conversion

--- 12_DateTime/conversion.cpp
+++ 12_DateTime/conversion.cpp
@@ -18,16 +18,26 @@ time_t dateToTimeStamp(std::string dateString){
     memset(&tm, 0, sizeof(tm));
 
     strptime(dateString.c_str(), "%Y-%m-%d %T", &tm);
+    printf("annee : %d\n",tm.tm_year + 1900);
+    printf("mois  : %d\n",tm.tm_mon+1);
+    printf("jour  : %d\n",tm.tm_mday);
+    printf("heure : %d\n",tm.tm_hour);
+    printf("min   : %d\n",tm.tm_min);
+    printf("sec   : %d\n",tm.tm_sec);
+    printf("décalage : %d\n",tm.tm_isdst);
+    tm.tm_isdst = -1;    // Not set by strptime();
+
     return mktime(&tm);
 }
 
 
 int main(int argc, char **argv) {
 
-    std::string date = "2017-05-15 10:55:01";
+    std::string date = "2017-05-22 10:55:01";
     time_t retour, maintenant, difference;
 
     retour = dateToTimeStamp(date);
+    printf("TimeStamp : %d\n",(int)retour);
     time(&maintenant);
     difference = maintenant - retour;
 

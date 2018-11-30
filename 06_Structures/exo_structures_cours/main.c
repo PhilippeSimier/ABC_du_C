51ce073abec5c1e58bd19250b5b732516e854e43
philippeSimier <philaure@wanadoo.fr>
2018-11-30 15:09:35 +0100

    add exo cours

---
+++ 06_Structures/exo_structures_cours/main.c
@@ -0,0 +1,73 @@
+/* 
+ * File:   main.c
+ * Author: psimier
+ *
+ * Created on 28 novembre 2018, 10:58
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 255
+
+typedef struct {
+    char nom[MAX];
+    char prenom[MAX];
+    unsigned int annee;
+    float taille;
+    float poids;
+}typePersonne;
+
+void afficherPersonne(typePersonne toto){
+    printf("nom    : %s\n", toto.nom);
+    printf("année  : %d\n", toto.annee);
+}
+
+void afficherPersonnePtr(typePersonne *toto){
+    printf("nom    : %s\n", toto->nom);
+    printf("année  : %d\n", toto->annee);
+}
+
+typePersonne creerPersonne(){
+   
+    typePersonne toto;
+    printf("Donnez le nom : \n");
+    scanf("%s", toto.nom );
+    printf("Donnez l'année de naissance : \n");
+    scanf("%d", &toto.annee );
+    
+    return toto;
+}
+
+typePersonne *creerPersonnePtr(){
+   
+    typePersonne *personne;
+    personne = (typePersonne *)malloc(sizeof(typePersonne));
+    printf("Donnez le nom : \n");
+    scanf("%s", personne->nom );
+    printf("Donnez l'année de naissance : \n");
+    scanf("%d", &personne->annee );
+    
+    return personne;
+}
+
+
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    typePersonne robert, jumeau;
+    typePersonne *raymonde;
+    
+    robert = creerPersonne();
+    raymonde = creerPersonnePtr();
+    afficherPersonne(robert);
+    
+    afficherPersonnePtr(raymonde);
+    
+
+    return (EXIT_SUCCESS);
+}
+
